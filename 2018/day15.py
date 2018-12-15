4a75af4c4a3edb5180427c847efef2ad89d0ac73
Tishka17 <tishka17@mail.ru>
2018-12-15 14:20:39 +0300

    iterate until enemies die

--- 2018/day15.py
+++ 2018/day15.py
@@ -3,16 +3,23 @@
 from enum import Enum
 from typing import List, Tuple, Dict, Optional
 from copy import deepcopy
+
+import itertools
+
+import sys
 from dataclasses import dataclass, field
 
 cave = """
 #######
-#.G.E.#
-#E.G.E#
-#.G.E.#
+#E.G#.#
+#.#G..#
+#G.#.G#
+#G..#.#
+#...E.#
 #######
 """.splitlines(keepends=False)
 
+#
 with open("in15.txt") as f:
     cave = f.read().splitlines(keepends=False)
 
@@ -20,6 +27,8 @@ BOLD = "\033[1m"
 DIM = "\033[90m"
 BLUE = "\033[36m"
 GREEN = "\033[32m"
+YELLOW = "\033[93m"
+MAGENTA = "\033[35m"
 RED = "\033[91m"
 NORMAL = "\033[0m"
 
@@ -40,6 +49,11 @@ class Unit:
     nation: Nation = field(hash=False)
     x: int = field(hash=False)
     y: int = field(hash=False)
+    power: int = field(hash=False, default=3)
+    hit_points: int = field(hash=False, default=200)
+
+    def __str__(self):
+        return f"{self.nation.value}[{self.id_}] at ({self.x},{self.y}): {self.hit_points}"
 
 
 Cave = List[List[str]]
@@ -47,11 +61,15 @@ Units = List[Unit]
 Path = List[Tuple[int, int]]
 
 
-def sort_units(units: List[Unit]):
-    return units.sort(key=lambda u: (u.x, u.y))
+def sort_units(units: List[Unit], use_hp=False):
+    return units.sort(key=lambda u: (u.hit_points if use_hp else 0, u.x, u.y))
 
 
-def print_map(cave: Cave, units: Units, curent: Unit = None, enemy: Unit = None, path: Path=[]):
+def clear():
+    print("\033c")
+
+
+def print_map(cave: Cave, units: Units, curent: Unit = None, enemy: Unit = None, path: Path = []):
     for x, row in enumerate(cave):
         for y, point in enumerate(row):
             for u in units:
@@ -60,12 +78,20 @@ def print_map(cave: Cave, units: Units, curent: Unit = None, enemy: Unit = None,
                         print(BOLD + u.nation.value + NORMAL, end="")
                     elif u == enemy:
                         print(RED + BOLD + u.nation.value + NORMAL, end="")
+                    elif 50 <= u.hit_points < 100:
+                        print(GREEN + u.nation.value + NORMAL, end="")
+                    elif 10 <= u.hit_points < 50:
+                        print(YELLOW + u.nation.value + NORMAL, end="")
+                    elif 0 < u.hit_points < 10:
+                        print(MAGENTA + u.nation.value + NORMAL, end="")
+                    elif u.hit_points <= 0:
+                        print(MAGENTA + "X" + NORMAL, end="")
                     else:
                         print(u.nation.value, end="")
                     break
             else:
                 if isinstance(point, str):
-                    if (x,y) in path:
+                    if (x, y) in path:
                         print(GREEN + "+" + NORMAL, end="")
                     else:
                         print(DIM + point + NORMAL, end="")
@@ -119,15 +145,18 @@ def find_targets(unit: Unit, cave: Cave, units: Units) -> (Optional[Unit], Path)
     targets: Dict[Units, Path] = {}
     # calculate distances
     while points:
+        points.sort(key=lambda p: p[2])
         new_points = []
         for x, y, path in points:
-            res = use_point(map, new_points, enemy, units, targets, x + 1, y, path)
+            res = False
             res |= use_point(map, new_points, enemy, units, targets, x - 1, y, path)
-            res |= use_point(map, new_points, enemy, units, targets, x, y + 1, path)
             res |= use_point(map, new_points, enemy, units, targets, x, y - 1, path)
+            res |= use_point(map, new_points, enemy, units, targets, x, y + 1, path)
+            res |= use_point(map, new_points, enemy, units, targets, x + 1, y, path)
         points = new_points
 
     # print_map(map, units, u)
+    # print()
     # select target
     selected_targets: Units = []
     distance = 0
@@ -141,18 +170,78 @@ def find_targets(unit: Unit, cave: Cave, units: Units) -> (Optional[Unit], Path)
 
     sort_units(selected_targets)
     if not selected_targets:
-        return None, 0
+        return None, []
     return selected_targets[0], targets[selected_targets[0]][1:]
 
 
-sort_units(units)
-for u in units:
-    print(u)
-    target, path = find_targets(u, cave, units)
-    # move
-    if path:
-        u.x, u.y = path[0]
-    print(u)
-    print_map(cave, units, u, target, path)
-    print(f"{len(path)}:", target)
+def find_attacked(unit: Unit, units: Units) -> Unit:
+    res = []
+    for u in units:
+        if u.nation == unit.nation.enemy() and u.hit_points > 0 and (
+                (u.x == unit.x and abs(u.y - unit.y)) == 1 or (u.y == unit.y and abs(u.x - unit.x) == 1)
+        ):
+            res.append(u)
+    if res:
+        sort_units(res, use_hp=True)
+        return res[0]
+    return None
+
+
+def check_enemies(unit: Unit, units: Units) -> bool:
+    for u in units:
+        if u.nation == unit.nation.enemy() and u.hit_points > 0:
+            return True
+    return False
+
+
+def print_hp(units: Units):
+    oldx = units[0].x
+    for u in units:
+        if u.x != oldx:
+            print()
+            oldx = u.x
+        print(f"{str(u):20}", end="\t")
     print()
+
+
+for n in itertools.count():
+    print()
+    print(n)
+    print_map(cave, units)
+    units = [u for u in units if u.hit_points > 0]
+    sort_units(units)
+    print_hp(units)
+    for u in units:
+        if u.hit_points <= 0:
+            continue
+        if not check_enemies(u, units):
+            print("Finish")
+            break
+        # print(u)
+        # search path to nearest target
+        target, path = find_targets(u, cave, units)
+
+        # move
+        if path:
+            u.x, u.y = path[0]
+        # print(u)
+        # print_map(cave, units, u, target, path)
+        # print()
+        # print(f"move {len(path)}:", target)
+
+        # select to attack
+        target = find_attacked(u, units)
+        # print(f"attack:", target)
+        if target:
+            target.hit_points -= u.power
+            # print(f"after attack:", target)
+        # print()
+    else:
+        continue
+    break
+
+print()
+print_map(cave, units)
+print_hp(units)
+total_hp = sum(u.hit_points for u in units if u.hit_points > 0)
+print(n, total_hp, n * total_hp)
